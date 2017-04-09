128d30b035420aa4f9dff92efef588c072b78b8f
Wojtek Okoński <wojteko22.96@gmail.com>
2017-04-09 14:39:44 +0200

    Init

---
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab2/lab2/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.wojciech.okonski.lab2.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
