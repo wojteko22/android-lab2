e1dd096c8b867a6cd6f0b9b1d57027f26ec39479
Wojtek Okoński <wojteko22.96@gmail.com>
2017-05-01 17:50:37 +0200

    Add small corrections

--- app/src/main/java/pl/edu/pwr/wojciech/okonski/lab2/lab2/MainActivity.kt
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab2/lab2/MainActivity.kt
@@ -56,10 +56,7 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
     }
 
     val swipeToDismissTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
-        override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
-            return false
-        }
-
+        override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?) = false
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             deleteItem(viewHolder)
         }
