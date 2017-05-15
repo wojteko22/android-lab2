7a5ffa37cb0e8575d810087dc692c5e420c7d9fa
Wojtek Okoński <wojteko22.96@gmail.com>
2017-05-15 08:57:08 +0200

    Keep fragment after rotation

--- app/src/main/java/pl/edu/pwr/wojciech/okonski/lab2/lab2/MovieActivity.kt
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab2/lab2/MovieActivity.kt
@@ -1,5 +1,6 @@
 package pl.edu.pwr.wojciech.okonski.lab2.lab2
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import pl.edu.pwr.wojciech.okonski.lab2.lab2.fragments.MovieFragment
@@ -8,11 +9,21 @@ class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
-        val fragment = MovieFragment()
-        fragment.arguments = intent.extras
+        addMovieFragmentIfThereIsNotInner()
+    }
+
+    private fun addMovieFragmentIfThereIsNotInner() {
+        val innerFragment: Fragment? = fragmentManager.findFragmentById(R.id.upper_inner_container)
+        if (innerFragment == null)
+            addMovieFragment()
+    }
+
+    private fun addMovieFragment() {
+        val movieFragment = MovieFragment()
+        movieFragment.arguments = intent.extras
         val transaction = fragmentManager.beginTransaction()
         with(transaction) {
-            add(R.id.outer_container, fragment)
+            add(R.id.outer_container, movieFragment)
             commit()
         }
     }
